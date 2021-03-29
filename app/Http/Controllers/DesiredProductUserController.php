98d67db44194efd46bb82cb17ee570db02c1138e
igoryan20 <igoryan1dobro@gmail.com>
2021-03-29 13:30:59 +0300

    finish pagination

---
+++ app/Http/Controllers/DesiredProductUserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DesiredProductUserController extends Controller
+{
+    public function getUsers() {
+
+
+        return view('pages/');
+    }
+}
